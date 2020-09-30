9eab43380e4f6a85c6a76f8dc41559c28fc740ba
willfbren <willfbren@gmail.com>
2020-09-29 22:43:01 -0500

    🛠

--- singly-linked-lists/singly-linked-list.js
+++ singly-linked-lists/singly-linked-list.js
@@ -118,4 +118,45 @@ class SinglyLinkedList {
         this.length++;
         return true;
     }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+
+        let prev = this.get(index - 1);
+        let removed = prev.next;
+        prev.next = removed.next;
+        this.length--;
+        return removed;
+    }
+
+    reverse() {
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        let next;
+        let prev = null;
+
+        for (let i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+
+        return this
+    }
+
+    print() {
+        let arr = [];
+        let current = this.head;
+
+        while(current) {
+            arr.push(current.val)
+            current = current.next;
+        }
+
+        console.log(arr)
+    }
 }
