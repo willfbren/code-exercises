8e837c5346016b05c572e107af43929889ddf544
willfbren <willfbren@gmail.com>
2020-10-02 16:29:39 -0500

    🛠

---
+++ doubly-linked-list/doubly-linked-list.js
@@ -0,0 +1,51 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (!this.head) return undefined;
+        let poppedNode = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = poppedNode.prev;
+            this.tail.next = null;
+            poppedNode.prev = null;
+        }
+
+        this.length--;
+        return poppedNode;
+    }
+}
+
+first = new Node(12);
+first.next = new Node(13);
+first.next.prev = first;
+
+console.log(first);
