ce37ecbdda628dec642b46478b6d9ee6637f8113
willfbren <willfbren@gmail.com>
2020-10-02 21:31:45 -0500

    🛠

--- doubly-linked-list/doubly-linked-list.js
+++ doubly-linked-list/doubly-linked-list.js
@@ -42,10 +42,58 @@ class DoublyLinkedList {
         this.length--;
         return poppedNode;
     }
+
+    shift() {
+        if (this.length === 0) return undefined;
+
+        let oldHead = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+
+        this.length--;
+        return oldHead;
+    }
+
+    unshift(val) {
+        let newNode = new Node(val);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+        let count, current;
+        if (index <= this.length / 2) {
+            count = 0;
+            current = this.head;
+            while (count !== index) {
+                current = current.next;
+                count++;
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while (count !== index) {
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+    }
 }
-
-first = new Node(12);
-first.next = new Node(13);
-first.next.prev = first;
-
-console.log(first);
