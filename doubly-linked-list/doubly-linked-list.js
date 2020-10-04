95e35a4e1903e83ca9b4e841fd386d5f1c6b97af
willfbren <willfbren@gmail.com>
2020-10-03 22:26:09 -0500

    🛠

--- doubly-linked-list/doubly-linked-list.js
+++ doubly-linked-list/doubly-linked-list.js
@@ -96,4 +96,46 @@ class DoublyLinkedList {
         }
         return current;
     }
+
+    set(index, val) {
+        let setNode = this.get(index);
+        if (setNode != null) {
+            setNode.val = val;
+            return true;
+        }
+        return false;
+    }
+
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === 0) return !!this.unshift(val);
+        if (index === this.length) return !!this.push(val);
+
+        let newNode = new Node(val);
+        let beforeNode = this.get(index - 1);
+        let afterNode = beforeNode.next;
+
+        beforeNode.next = newNode;
+        newNode.prev = beforeNode;
+        newNode.next = afterNode;
+        afterNode.prev = newNode;
+
+        this.length++;
+        return true;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+
+        let removedNode = this.get(index);
+        removedNode.prev.next = removedNode.next;
+        removedNode.next.prev = removedNode.prev;
+        removedNode.next = null;
+        removedNode.prev = null;
+
+        this.length--;
+        return removedNode;
+    }
 }
