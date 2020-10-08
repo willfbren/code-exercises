65ade4e0da937609184644029c2f3f6e779e7a09
willfbren <willfbren@gmail.com>
2020-10-07 21:56:53 -0500

    🛠

---
+++ binary-trees/binary-search-tree.js
@@ -0,0 +1,68 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value){
+        let newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        let current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                } 
+                current = current.right;
+            }
+        }
+    }
+
+    // more verbose with explicit else statements
+
+    // insert(value) {
+    //     let newNode = new Node(value);
+    //     if (this.root === null) {
+    //         this.root = newNode;
+    //         return this;
+    //     } else {
+    //         let current = this.root;
+    //         while (true) {
+    //             if (value === current.value) return undefined;
+    //             if (value < current.value) {
+    //                 if (current.left === null) {
+    //                     current.left = newNode;
+    //                     return this;
+    //                 } else {
+    //                     current = current.left;
+    //                 }
+    //             } else if (value > current.value) {
+    //                 if (current.right === null) {
+    //                     current.right = newNode;
+    //                     return this;
+    //                 } else {
+    //                     current = current.right;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+}
