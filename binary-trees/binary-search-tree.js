d48f5513e6ab637153dbeb1b1b0ac7c70472d4cc
willfbren <willfbren@gmail.com>
2020-10-08 20:58:07 -0500

    🛠

--- binary-trees/binary-search-tree.js
+++ binary-trees/binary-search-tree.js
@@ -11,26 +11,47 @@ class BinarySearchTree {
         this.root = null;
     }
 
-    insert(value){
+    find(value) {
+        if (this.root === null) return false;
+        let current = this.root,
+            found = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                // if we just want to return true or false
+                // return true
+                found = true;
+            }
+        }
+        // if we just want to return true or false
+        // return false
+        if (!found) return undefined;
+        return current;
+    }
+
+    insert(value) {
         let newNode = new Node(value);
-        if(this.root === null){
+        if (this.root === null) {
             this.root = newNode;
             return this;
         }
         let current = this.root;
-        while(true){
-            if(value === current.value) return undefined;
-            if(value < current.value){
-                if(current.left === null){
+        while (true) {
+            if (value === current.value) return undefined;
+            if (value < current.value) {
+                if (current.left === null) {
                     current.left = newNode;
                     return this;
                 }
                 current = current.left;
             } else {
-                if(current.right === null){
+                if (current.right === null) {
                     current.right = newNode;
                     return this;
-                } 
+                }
                 current = current.right;
             }
         }
