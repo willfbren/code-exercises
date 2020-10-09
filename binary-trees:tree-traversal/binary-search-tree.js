5e02c617cff5873cc96039bcdcda3c6eec484d61
willfbren <willfbren@gmail.com>
2020-10-08 21:37:02 -0500

    🛠

--- binary-trees:tree-traversal/binary-search-tree.js
+++ binary-trees:tree-traversal/binary-search-tree.js
@@ -86,4 +86,21 @@ class BinarySearchTree {
     //         }
     //     }
     // }
+
+    bfs() {
+        let data = [];
+        let queue = [];
+        let node = this.root;
+
+        queue.push(this.root);
+
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        return data;
+    }
 }
