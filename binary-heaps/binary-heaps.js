997ba595edae2ca275342ff7db56699910526438
willfbren <willfbren@gmail.com>
2020-10-12 15:31:13 -0500

    🛠

--- binary-heaps/binary-heaps.js
+++ binary-heaps/binary-heaps.js
@@ -4,3 +4,77 @@
 
 // for any child node at index n...
 // its parent is at index (n - 1) / 2 (floored - just chop of the decimal)
+
+class MaxBinaryHeap {
+    constructor() {
+        this.values = [];
+    }
+
+    insert(element) {
+        this.values.push(element);
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+        let idx = this.values.length - 1;
+        // newly inserted element
+        const element = this.values[idx];
+
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parent = this.values[parentIdx];
+            if (element <= parent) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+
+    extractMax() {
+        const max = this.values[0];
+        const end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            // trickle down
+            this.sinkDown();
+        }
+
+        return max;
+    }
+
+    sinkDown() {
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+
+        while (true) {
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild > element) {
+                    swap = leftChildIdx;
+                }
+            }
+
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if (
+                    (swap === null && rightChild > element) ||
+                    (swap !== null && rightChild > leftChild)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = this.values[element];
+            idx = swap;
+        }
+    }
+}
