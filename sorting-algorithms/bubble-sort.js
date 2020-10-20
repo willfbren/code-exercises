4c1f51ba68237f3c34e25821f5c7494b8779e5dc
willfbren <willfbren@gmail.com>
2020-10-19 22:57:03 -0500

    🛠

---
+++ sorting-algorithms/bubble-sort.js
@@ -0,0 +1,43 @@
+function bubbleSort(arr) {
+    let noSwaps;
+    for (let i = arr.length; i > 0; i--) {
+        noSwaps = true;
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // swap
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                noSwaps = false;
+            }
+        }
+        if (noSwaps) break;
+    }
+
+    return arr;
+}
+
+
+
+// with ES2015 function
+function bubbleSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+
+    let noSwaps;
+    for (let i = arr.length; i > 0; i--) {
+        noSwaps = true;
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+                noSwaps = false;
+            }
+        }
+        if (noSwaps) break;
+    }
+
+    return arr;
+}
+
+bubbleSort([37, 45, 29, 8]);
