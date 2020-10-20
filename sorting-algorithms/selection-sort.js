0572604023a6de29776ed433d45d22f6d0a05510
willfbren <willfbren@gmail.com>
2020-10-20 17:58:01 -0500

    🛠

---
+++ sorting-algorithms/selection-sort.js
@@ -0,0 +1,36 @@
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if (i !== min) {
+            let temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    return arr;
+}
+
+// ES2015 function call swap
+function selectionSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if (i !== min) swap(arr, i, min);
+    }
+
+    return arr;
+}
