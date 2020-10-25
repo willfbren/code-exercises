a24423d2380a305afc8ac8bd7208c3086817f127
willfbren <willfbren@gmail.com>
2020-10-24 20:11:23 -0500

    🛠

---
+++ sorting-algorithms/merge-sort.js
@@ -0,0 +1,29 @@
+function merge(arr1, arr2) {
+    let results = [];
+    let i = 0,
+        j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr2[j] > arr1[i]) {
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    }
+
+    return results;
+}
+
+merge([1, 5, 8], [2, 4, 7, 10])
