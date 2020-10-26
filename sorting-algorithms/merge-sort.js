7d4797a6c9be78985144d7842e17e468e1c93f6b
willfbren <willfbren@gmail.com>
2020-10-25 21:53:44 -0500

    🛠

--- sorting-algorithms/merge-sort.js
+++ sorting-algorithms/merge-sort.js
@@ -26,4 +26,14 @@ function merge(arr1, arr2) {
     return results;
 }
 
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+
+    return merge(left, right);
+}
+
 merge([1, 5, 8], [2, 4, 7, 10])
