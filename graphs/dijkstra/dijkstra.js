58108f94da9871224fd2f6ec7a0667708ab01de6
willfbren <willfbren@gmail.com>
2020-11-08 22:44:15 -0600

    🪂

--- graphs/dijkstra/dijkstra.js
+++ graphs/dijkstra/dijkstra.js
@@ -14,3 +14,22 @@ class WeightedGraph {
         this.adjacencyList[v2].push({ node: v1, weight });
     }
 }
+
+class PriorityQueue {
+    constructor() {
+        this.values = [];
+    }
+
+    enqueue(val, priority) {
+        this.values.push({ val, priority });
+        this.sort();
+    }
+
+    dequeue() {
+        return this.values.shift();
+    }
+
+    sort() {
+        this.values.sort((a, b) => a.priority - b.priority);
+    }
+}
