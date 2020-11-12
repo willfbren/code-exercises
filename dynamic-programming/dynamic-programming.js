2098e40e957ee0f201376ea980ced8a85715b929
willfbren <willfbren@gmail.com>
2020-11-11 23:01:54 -0600

    🪂

--- dynamic-programming/dynamic-programming.js
+++ dynamic-programming/dynamic-programming.js
@@ -9,7 +9,7 @@ function fib(n) {
     return fib(n - 1) + fib(n - 2);
 }
 
-// fibonacci with dp (memoization)
+// fibonacci with dp (memoization) - top down approach
 // time complexity -> O(n)
 function fib(n, memo = []) {
     if (memo[n] !== undefined) return memo[n];
@@ -17,4 +17,16 @@ function fib(n, memo = []) {
     let res = fib(n - 1, memo) + fib(n - 2, memo);
     memo[n] = res;
     return res;
+}
+
+// fibonacci using tabulation - bottom up approach
+function fib(n) {
+    if (n <= 2) return 1;
+    let fibNums = [0, 1, 1];
+
+    for (let i = 3; i <= n; i++) {
+        fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+    }
+
+    return fibNums[n]
 }
