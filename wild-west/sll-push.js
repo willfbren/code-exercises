b71382270f0e42db28bc0fc3d9d440219ddcc0d2
willfbren <willfbren@gmail.com>
2020-11-12 23:47:28 -0600

    🇽🇰

---
+++ wild-west/sll-push.js
@@ -0,0 +1,24 @@
+// Implement the following on the SinglyLinkedList class:
+// push
+
+// This function should take in a value and add a node to the end of the SinglyLinkedList. It should return the SinglyLinkedList.
+
+// Examples
+let singlyLinkedList = new SinglyLinkedList();
+singlyLinkedList.push(5); //singlyLinkedList
+singlyLinkedList.length; // 1
+singlyLinkedList.head.val; // 5
+singlyLinkedList.tail.val; // 5
+ 
+singlyLinkedList.push(10); // singlyLinkedList
+singlyLinkedList.length; // 2
+singlyLinkedList.head.val; // 5
+singlyLinkedList.head.next.val; // 10
+singlyLinkedList.tail.val; // 10
+
+singlyLinkedList.push(15); //singlyLinkedList
+singlyLinkedList.length; // 3
+singlyLinkedList.head.val; // 5
+singlyLinkedList.head.next.val; // 10
+singlyLinkedList.head.next.next.val; //15
+singlyLinkedList.tail.val; //15
