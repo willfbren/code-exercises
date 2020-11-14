06bf4fb43752392398271eb59f9a1cb6d6aa0322
willfbren <willfbren@gmail.com>
2020-11-13 23:26:52 -0600

    emoji

---
+++ wild-west/sll-pop.js
@@ -0,0 +1,35 @@
+// Implement the following on the SinglyLinkedList.prototype: pop
+
+// This function should remove a node at the end of the SinglyLinkedList. It should return the node removed.
+
+// Examples
+
+let singlyLinkedList = new SinglyLinkedList();
+
+singlyLinkedList.push(5); // singlyLinkedList
+singlyLinkedList.length; // 1
+singlyLinkedList.head.val; // 5
+singlyLinkedList.tail.val; // 5
+
+singlyLinkedList.push(10); // singlyLinkedList
+singlyLinkedList.length; // 2
+singlyLinkedList.head.val; // 5
+singlyLinkedList.head.next.val; // 10
+singlyLinkedList.tail.val; // 10
+
+singlyLinkedList.push(15); // singlyLinkedList
+singlyLinkedList.length; // 3
+singlyLinkedList.head.val; // 5
+singlyLinkedList.head.next.val; // 10
+singlyLinkedList.head.next.next.val; // 15
+singlyLinkedList.tail.val; // 15
+
+singlyLinkedList.pop().val; // 15
+singlyLinkedList.tail.val; // 10
+singlyLinkedList.length; // 2
+singlyLinkedList.pop().val; // 10
+singlyLinkedList.length; // 1
+singlyLinkedList.pop().val; // 5
+singlyLinkedList.length; // 0
+singlyLinkedList.pop(); // undefined
+singlyLinkedList.length;
